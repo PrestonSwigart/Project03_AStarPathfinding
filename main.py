9781a4b89e8658bd5dcff3d91533b38896203562
Preston Swigart <prestonlswigart@gmail.com>
2024-11-21 20:27:18 -0500

    THANK YOU TECH WITH TIM MY SAVIOR

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
-
+from queue import PriorityQueue
 
 #initial variables
 WIDTH = 700
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Pathfinding Visualizer")
 
+#initial colors
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 255, 0)
@@ -17,6 +18,7 @@ ORANGE = (255, 165 ,0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
+#node class (called spot because i followed a tutorial and thats what they called it)
 class Spot:
 	#constructor
 	def __init__(self, row, col, width, total_rows):
@@ -86,16 +88,17 @@ class Spot:
 		if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # LEFT
 			self.neighbors.append(grid[self.row][self.col - 1])
 
+	# "less than" method, compareTo from java
 	def __lt__(self, other):
 		return False
 
-#basic heuristics
-def heuristicVal(p1, p2):
-	x1, y1 = p1
-	x2, y2 = p2
+#manhattan distance
+def heuristicValue(p1, p2):
+	x1, y1 = p1 #pos 1
+	x2, y2 = p2 #pos 2
 	return abs(x1 - x2) + abs(y1 - y2)
 
-
+#creates the grid of Spots
 def make_grid(rows, width):
 	grid = []
 	gap = width // rows
@@ -131,31 +134,79 @@ def get_clicked_pos(pos, rows, width):
 	gap = width // rows
 	y, x = pos
 
-	row = y // gap
-	col = x // gap
+	return y // gap, x // gap
 
-	return row, col
+#makes cool animation at end
+def reconstruct_path(came_from, current, draw):
+	while current in came_from:
+		current = came_from[current]
+		current.make_path()
+		draw()
 
-def aStar():
-	return
+#the actual algorithm!
+def aStarAlg(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	g_score = {spot: float("inf") for row in grid for spot in row}
+	g_score[start] = 0
+	f_score = {spot: float("inf") for row in grid for spot in row}
+	f_score[start] = heuristicValue(start.get_pos(), end.get_pos())
 
+	open_set_hash = {start}
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
+
+		if current == end:
+			reconstruct_path(came_from, end, draw)
+			return True
+
+		for neighbor in current.neighbors:
+			temp_g_score = g_score[current] + 1
+
+			if temp_g_score < g_score[neighbor]:
+				g_score[neighbor] = temp_g_score
+				came_from[neighbor] = current
+				f_score[neighbor] = temp_g_score + heuristicValue(neighbor.get_pos(), end.get_pos())
+				if neighbor not in open_set_hash:
+					count += 1
+					open_set.put((f_score[neighbor], count, neighbor))
+					open_set_hash.add(neighbor)
+					neighbor.make_open()
+
+		draw()
+		if current != start:
+			current.make_closed()
+
+	return False
+
+
+
+#main method for setting up the program and running it
 def main(win, width):
-	ROWS = 20
-	grid = make_grid(ROWS, width)
+	rows = 50
+	grid = make_grid(rows, width)
 
 	start = None
 	end = None
 
-	run = True
-	while run:
-		draw(win, grid, ROWS, width)
+	crashed = False
+	while not crashed:
+		draw(win, grid, rows, width)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				run = False
+				crashed = True
 
 			if pygame.mouse.get_pressed()[0]: # LEFT
 				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
+				row, col = get_clicked_pos(pos, rows, width)
 				spot = grid[row][col]
 				if not start and spot != end:
 					start = spot
@@ -170,7 +221,7 @@ def main(win, width):
 
 			elif pygame.mouse.get_pressed()[2]: # RIGHT
 				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
+				row, col = get_clicked_pos(pos, rows, width)
 				spot = grid[row][col]
 				spot.reset()
 				if spot == start:
@@ -183,13 +234,14 @@ def main(win, width):
 					for row in grid:
 						for spot in row:
 							spot.update_neighbors(grid)
-					#the algorithm goes here
+					aStarAlg(lambda: draw(win, grid, rows, width), grid, start, end)
 
 
+				#clears on pressing C
 				if event.key == pygame.K_c:
 					start = None
 					end = None
-					grid = make_grid(ROWS, width)
+					grid = make_grid(rows, width)
 
 	pygame.quit()
 
